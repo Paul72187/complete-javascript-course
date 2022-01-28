9f1ad135c11fa78ac32c8cb7d9c94193338db02c
Paul Cooper <87516199+Paul72187@users.noreply.github.com>
2022-01-28 10:04:57 +0000

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,13 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
 
+console.log("Janet");
+console.log(23);
+
+let firstName = "Janet";
+console.log(firstName);
+
+let PI = 3.1415;
+
+let myFirstJob = 'estate agent';
+let myCurrentJob = 'supermarket assistant';
